88b703dfeb2b9ff7e18c2fe3fdbb57e8d70ea0bf
pablobt99 <64027415+pablobt99@users.noreply.github.com>
2021-05-30 19:13:49 +0200

    changes

---
+++ EcoMvcObserver/Departments/IT.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EcoMvcObserver.Departments
+{
+    public interface IAsset
+    {
+        void AllocateAsset();
+    }
+    public class IT: IAsset
+    {
+        public void AllocateAsset()
+        {
+
+        }
+    }
+}
---
+++ EcoMvcObserver/Subject/IResignation.cs
@@ -0,0 +1,11 @@
+﻿using Observer.Departments;
+
+namespace Observer.Subject
+{
+    public interface IResignation
+    {
+        void AddObserver(IResignationObserver observer);
+        void RemoveObserver(IResignationObserver observer);
+        void NotifyObserver(string employeeId);
+    }
+}
