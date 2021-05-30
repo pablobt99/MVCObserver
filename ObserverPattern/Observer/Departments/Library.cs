46c3f8e03a710ad94e709ede4a17e29661603cda
pablobt99 <64027415+pablobt99@users.noreply.github.com>
2021-05-30 19:07:11 +0200

    Initial

---
+++ ObserverPattern/Observer/Departments/Library.cs
@@ -0,0 +1,31 @@
+﻿using Observer.Subject;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Observer.Departments
+{
+    public interface ILibrary
+    {
+        void ManageBook();
+    }
+    public class Library : ILibrary, IResignationObserver
+    {
+        public Library(IResignation resignation)
+        {
+            resignation.AddObserver(this);
+        }
+
+        public void ManageBook()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Notify(string employeeId)
+        {
+            UpdateXmlHelper xmlHelper = new UpdateXmlHelper();
+            xmlHelper.UpdateNotificationDetail("Library", employeeId);
+        }
+    }
+}
---
+++ ObserverPattern/Observer/Subject/IResignation.cs
@@ -0,0 +1,15 @@
+﻿using Observer.Departments;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Observer.Subject
+{
+    public interface IResignation
+    {
+        void AddObserver(IResignationObserver observer);
+        void RemoveObserver(IResignationObserver observer);
+        void NotifyObserver(string employeeId);
+    }
+}
