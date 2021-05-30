7ecc86af2de478acdd3e03beb541f5ea95a068cd
pablobt99 <64027415+pablobt99@users.noreply.github.com>
2021-05-30 21:24:49 +0200

    cambios

---
+++ ObserverPattern/Observer/Departments/Finance.cs
@@ -0,0 +1,29 @@
+﻿using Observer.Subject;
+
+namespace Observer.Departments
+{
+    public interface IFinance
+    {
+        void CalculateSalary();
+    }
+    public class Finance : IFinance, IResignationObserver
+    {
+        public Finance(IResignation resignation)
+        {
+            resignation.AddObserver(this);
+        }
+        public void CalculateSalary()
+        {
+            // calculates salary
+        }
+
+        public void Notify(string employeeId)
+        {
+            // whenever employee resigns notification will come here.
+            // Finance department will take necessary action accordingly.
+
+            UpdateXmlHelper xmlHelper = new UpdateXmlHelper();
+            xmlHelper.UpdateNotificationDetail("Finance", employeeId);
+        }
+    }
+}
---
+++ ObserverPattern/Observer/Departments/IT.cs
@@ -0,0 +1,29 @@
+﻿using Observer.Subject;
+
+namespace Observer.Departments
+{
+    public interface IAsset
+    {
+        void AllocateAsset();
+    }
+    public class IT : IAsset, IResignationObserver
+    {
+        public IT(IResignation resignation)
+        {
+            resignation.AddObserver(this);
+        }
+        public void AllocateAsset()
+        {
+            // Allocate asset
+        }
+
+        public void Notify(string employeeId)
+        {
+            // whenever employee resigns notification will come here.
+            // Finance department will take necessary action accordingly.
+
+            UpdateXmlHelper xmlHelper = new UpdateXmlHelper();
+            xmlHelper.UpdateNotificationDetail("IT", employeeId);
+        }
+    }
+}
